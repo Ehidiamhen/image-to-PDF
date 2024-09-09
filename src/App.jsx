b70a7e5dfde39dc564903d5b3511fbf4f7ebe995
Aigbiluese Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2024-09-09 18:04:38 +0100

    implemented basic conversion of images to pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,61 @@
-import { useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
+import jsPDF from 'jspdf'
 import './App.css'
 
 function App() {
   const fileInputRef = useRef(null)
+  const [images, setImages] = useState([])
 
   const handleCLick = () => {
     fileInputRef.current.click()
   }
 
-  // const handleFileChange = (event) => {
-  //   const file = event.target.files[0]
-  //   if (file) {
-  //     console.log('Selected file:', file)
-  //   }
-  // }
+  const handleFileChange = (e) => {
+    setImages(e.target.files)
+  }
+
+  const handleConvert = () => {
+    if (!images.length) return
+
+    const pdf = new jsPDF()
+
+    Array.from(images).forEach((file, index) =>{
+      const reader = new FileReader()
+      reader.onload = function(event) {
+        const img = new Image()
+        img.src = event.target.result
+
+        img.onload = function() {
+          const imgWidth = pdf.internal.pageSize.getWidth()
+          const imgHeight = (img.height * imgWidth) / img.width
+
+          if (index > 0) pdf.addPage()
+          pdf.addImage(img, 'JPEG', 0, 0, imgWidth, imgHeight)
+          
+          if (index === images.length -1) {
+            pdf.save('converted.pdf')
+          }
+        }
+      }
+      reader.readAsDataURL(file)
+    })
+  }
+  
   return (
-    // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>ImageToPDF</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
     <>
-      <h2>JPG TO PDF</h2>
-      <p>Convert JPG images to PDF in seconds. Easily adjust orientation and margins.</p>
-      <button onClick={handleCLick}>Select JPG images</button>
+      <h2>IMAGE TO PDF</h2>
+      <p>Convert images to PDF in seconds. Easily adjust orientation and margins.</p>
+      {(images.length == 0 )
+        ?<button onClick={handleCLick}>Select your images</button>
+        :<button onClick={handleConvert}>Convert to PDF</button>
+      }
       <input 
       type="file" 
       ref={fileInputRef}
       style={{display: 'none'}}
       accept='image/*'
-      // onChange={handleFileChange}
-      id="" 
+      multiple
+      onChange={handleFileChange}
       />
       
     </>
